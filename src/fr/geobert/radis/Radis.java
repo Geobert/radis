741bf59f414fc9483957178d5b792fd9cdcd4e2d
geobert <none@none>
2011-07-29 19:47:30 +0200

    fix issue 72

--- src/fr/geobert/radis/Radis.java
+++ src/fr/geobert/radis/Radis.java
@@ -1,12 +1,14 @@
 package fr.geobert.radis;
 
-import org.acra.ACRA;
-import org.acra.ReportingInteractionMode;
-import org.acra.annotation.ReportsCrashes;
+import org.acra.*;
+import org.acra.annotation.*;
+import static org.acra.ReportField.*;
 
 import android.app.Application;
 
-@ReportsCrashes(formKey = "dG5RMmtwVFk0eEtLbThwcV9hU0UwZkE6MQ", mode = ReportingInteractionMode.NOTIFICATION, resNotifTickerText = R.string.crash_notif_ticker_text, resNotifTitle = R.string.crash_notif_title, resNotifText = R.string.crash_notif_text, resDialogTitle = R.string.crash_dialog_title, resDialogCommentPrompt = R.string.crash_dialog_comment_prompt, resDialogOkToast = R.string.crash_dialog_ok_toast, resDialogText = R.string.crash_dialog_text)
+@ReportsCrashes(formKey = "dENobzlzRGpYVmhLLWMzZUxvS1RkY1E6MQ", 
+		customReportContent = { APP_VERSION_CODE, APP_VERSION_NAME, PHONE_MODEL, BRAND, ANDROID_VERSION, STACK_TRACE, USER_COMMENT, BUILD, CUSTOM_DATA },
+		mode = ReportingInteractionMode.NOTIFICATION, resNotifTickerText = R.string.crash_notif_ticker_text, resNotifTitle = R.string.crash_notif_title, resNotifText = R.string.crash_notif_text, resDialogTitle = R.string.crash_dialog_title, resDialogCommentPrompt = R.string.crash_dialog_comment_prompt, resDialogOkToast = R.string.crash_dialog_ok_toast, resDialogText = R.string.crash_dialog_text)
 public class Radis extends Application {
 	RadisConfiguration cfg;
 	
@@ -16,7 +18,5 @@ public class Radis extends Application {
 		ACRA.init(this);
 		cfg = new RadisConfiguration();
 		super.onCreate();
-	}
-	
-	
+	}	
 }
